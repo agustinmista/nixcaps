cab52a33de3ef4d866796745fdcf907c26ba6210
Agustin Mista <agustin@mista.me>
2025-12-28 22:00:20 +0100

    Turn qmk_firmware into a flake input

--- templates/ergodox_ez/flake.nix
+++ templates/ergodox_ez/flake.nix
@@ -5,6 +5,9 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nixcaps.url = "github:agustinmista/nixcaps/main";
+    # Uncomment the next line if you want to use a specific revision of
+    # qmk_firmware. Just make sure not to remove the submodules=1 part.
+    # nixcaps.inputs.qmk_firmware.url = "git+https://github.com/qmk/qmk_firmware?submodules=1&rev=54e8fad959d6a6e53e08c62ac3a3c4d4bdc6c957";
   };
 
   outputs =
